c508a86453203f5d16e245b8a20c85d1a069ea47
Will Pisani <wapisani@mtu.edu>
2024-02-11 17:32:34 -0600

    Added scripting to get Pocket PC from Elm's Aide

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -460,6 +460,7 @@ AideScript_WalkPotion1:
 	applymovement ELMSLAB_ELMS_AIDE, AideWalksRight1
 	turnobject PLAYER, DOWN
 	scall AideScript_GivePotion
+        scall AideScript_GivePocketPC
 	applymovement ELMSLAB_ELMS_AIDE, AideWalksLeft1
 	end
 
@@ -467,6 +468,7 @@ AideScript_WalkPotion2:
 	applymovement ELMSLAB_ELMS_AIDE, AideWalksRight2
 	turnobject PLAYER, DOWN
 	scall AideScript_GivePotion
+        scall AideScript_GivePocketPC
 	applymovement ELMSLAB_ELMS_AIDE, AideWalksLeft2
 	end
 
@@ -478,8 +480,18 @@ AideScript_GivePotion:
 	writetext AideText_AlwaysBusy
 	waitbutton
 	closetext
-	setscene SCENE_ELMSLAB_NOOP
 	end
+
+AideScript_GivePocketPC:
+        opentext
+        writetext AideText_GetPocketPCText
+        promptbutton
+        giveitem POCKET_PC
+        writetext AideText_PocketPCInfoText
+        waitbutton
+        closetext
+        setscene SCENE_ELMSLAB_NOOP
+        end
 
 AideScript_WalkBalls1:
 	applymovement ELMSLAB_ELMS_AIDE, AideWalksRight1
@@ -1367,6 +1379,18 @@ ElmsLabPCText:
 	para "…It says on the"
 	line "screen…"
 	done
+
+AideText_GetPocketPCText:
+        text "Oh, I have this"
+        line "for you too."
+       
+        para "It's a Pocket PC!"
+        done
+
+AideText_PocketPCInfoText:
+        text "Use this to manage"
+        line "your party."
+        done
 
 ElmsLab_MapEvents:
 	db 0, 0 ; filler
